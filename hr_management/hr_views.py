452f77fb0fe3e1d42d54a2374a5b6994984a39cb
UMAIR271 <malikumair2701@gmail.com>
2023-02-01 01:00:46 +0500

    hr_services

--- hr_management/hr_views.py
+++ hr_management/hr_views.py
@@ -4,6 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from database.event import get_event_id
+from database.database_management import *
+from database.connection import dowellconnection
 import requests
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -11,3 +14,50 @@ class getServerReport(APIView):
 
     def get(self, request):
         return Response({"info": "APi services for Hr_view"},status=status.HTTP_200_OK)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class get_candidate_views(APIView):
+
+    def post(self, request):
+        data = request.data
+        field = {
+            "company_id":data.get('company_id','')
+
+        }
+        update_field = {
+            "status":"nothing to update"
+        }
+        response = dowellconnection(*candidate_management_reports,"fetch",field,update_field)
+        print(response)
+        if response:
+            return Response({"message":"List of job apllications.","response":json.loads(response)},status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"There is no job applications","response":json.loads(response)},status=status.HTTP_204_NO_CONTENT)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class hr_save_view(APIView):
+    def post(self, request):
+            data = request.data
+            print(data.get('id',''))
+            field = {
+                "_id":data.get('id',''),
+                }
+            fields = {
+                "eventId":get_event_id()['event_id'],
+                "company_id":data.get('company_id',''),
+                 "applicant ": data.get('applicant ', ''),
+                "hr_remarks ": data.get('hr_remarks ', ''),
+                "status":data.get('status ', ''),
+                }
+            update_field = {
+               "hr_remarks ": data.get('hr_remarks ', ''),
+                "status":data.get('status ', ''),
+            }
+            print("hello")
+            response = dowellconnection(*candidate_management_reports,"fetch",field,update_field)
+            print(response)
+            # response1 = dowellconnection(*hr_management_reports,"insert",fields,update_field)
+            # print(response1)
+            return Response({"message":"Job creation was successful."},status=status.HTTP_201_CREATED)
+
+            # return Response({"message":"Job creation has failed"},status=status.HTTP_400_BAD_REQUEST)
--- lead_management/lead_views.py
+++ lead_management/lead_views.py
@@ -4,6 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from database.event import get_event_id
+from database.database_management import *
+from database.connection import dowellconnection
 import requests
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -11,3 +14,29 @@ class getServerReport(APIView):
 
     def get(self, request):
         return Response({"info": "APi services for lead_view"},status=status.HTTP_200_OK)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class team_lead_view(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            field = {
+                "eventId":get_event_id()['event_id'],
+                "name" : data.get('name', ''),
+                "team_lead": data.get('team_lead', ''),
+                "discord_link": data.get('discord_link', ''),
+                "general_info":{
+                    "company_id":data.get('company_id',''),
+                    "data_type":data.get('data_type',''),
+                    "created_by":data.get('created_by','')
+                }
+                }
+
+            update_field = {
+                "status":"nothing to update"
+            }
+            response = dowellconnection(*lead_management_reports,"insert",field,update_field)
+            print(response)
+            return Response({"message":"Job creation was successful."},status=status.HTTP_201_CREATED)
+        except:
+            return Response({"message":"Job creation has failed"},status=status.HTTP_400_BAD_REQUEST)
