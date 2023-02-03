5cab3882284edd81ed9a68e81cdefdd707829098
alaasassii <alaasassijs@gmail.com>
2023-02-03 10:22:14 +0100

    add fetching options of selection

--- 100098-dowelljobportal/src/pages/ResearchAssociatePage/ResearchAssociatePage.jsx
+++ 100098-dowelljobportal/src/pages/ResearchAssociatePage/ResearchAssociatePage.jsx
@@ -1,4 +1,10 @@
-import "./style.css";
+import {useState , useEffect} from 'react' ; 
+// Loading Component 
+import Loading from './Loading';
+import LittleLoading from './littleLoading';
+// countries 
+import {continentsData} from '../../utils/testData'
+// images
 import logo from "./images/logo.png"
 import home from './icons/one.svg' ; 
 import two from './icons/two.svg' ; 
@@ -7,7 +13,55 @@ import search from './icons/search.svg';
 import selectorArrow from './icons/selectorArrow.svg'
 import map from './images/map.png' ; 
 import message from './icons/message.svg'
+import cities from './images/cities.png'
+// axios
+import axios from 'axios'
+// css
+import "./style.css";
+
 const ResearchAssociatePage = () => {
+    // continents 
+    const [continents , setContinents] = useState([]) ; 
+    const [continentValue , setcontinentValue] = useState("") ; 
+    // countries 
+    const [countries , setCountries] = useState(continentsData) ; 
+    const [countriesValue , setcountriesValue] = useState("") ; 
+    const [disableCountrie , setdisableCountrie] = useState(true) ; 
+    // regions 
+    const [region , setRegion] = useState([]) ;  
+    const [regionValue , setRegionValue] = useState("") ; 
+    const [disableregion , setdisableregion ] = useState(true) ; 
+    const [loadingRegion , setLoadingRegion] = useState(false)
+    // fetching data
+    useEffect(()=>{
+        // continents
+        axios("https://100074.pythonanywhere.com/continents/johnDoe123/haikalsb1234/100074/?format=json")
+        .then(response => {console.log("continents: ",response.data) ; setContinents(response.data)})
+        .catch(error => {console.log(error)})
+        
+    },[])
+        // regions 
+
+    useEffect(()=>{
+        if(continentValue){
+            setRegion([]) ;
+            console.log("Loading")
+            setLoadingRegion(true)
+            axios(`https://100074.pythonanywhere.com/region/name/${countriesValue}/johnDoe123/haikalsb1234/100074/`)
+            .then(response => {console.log("regions: ",response.data); setRegion(response.data) ; setLoadingRegion(false);setdisableregion(false)})
+            .catch(error => {console.log("error") ; setRegion([]) ; setLoadingRegion(false);setdisableregion(false)})
+        }
+    },[countriesValue])
+
+// disble element
+    useEffect(()=>{
+        if(continentValue)
+            setdisableCountrie(false)
+        if(countriesValue)
+            setdisableregion(false)
+    },[continentValue , countriesValue])
+
+    
     return <div className={"research-jobs"}>
     <header>
         <div>
@@ -23,44 +77,69 @@ const ResearchAssociatePage = () => {
         </ul>
     </nav>
     <main>
-        <div className="selections">
-            <div>
-                <h6>Country Name:</h6>
-                <div className="input">
-                <div>
-                <img src={search} alt="" />
-                <select placeholder="select an option" name="" id="">
-                    <option value="">select an option</option>
-                    <option value="">asd asd </option>
-                    <option value="">asdasd </option>
-                    <option value="">asdasd</option>
-                    <option value="">asdqw</option>
-                </select>
-                </div>
-                <img src={selectorArrow} alt="" />
-
-                </div>
-            </div>
-            <div>
-                <h6>City Name:</h6>
-                <div className="input">
-                <div>
-                <img src={search} alt="" />
-                <select placeholder="select an option" name="" id="">
-                    <option value="">select an option</option>
-                    <option value="">asd asd </option>
-                    <option value="">asdasd </option>
-                    <option value="">asdasd</option>
-                    <option value="">asdqw</option>
-                </select>
-                </div>
-                <img src={selectorArrow} alt="" />
-
-                </div>
-            </div>
-        </div>
+        {/* region could be back  */}
+        {   (continents.length ) ? 
+        <>
+                    <div className="selections">
+                    <div>
+                        <h6>Continent Name:</h6>
+                        <div className="input">
+                        <div>
+                        <img src={search} alt="" />
+                        <select defaultValue={"Select continent"} placeholder="select an option" onChange={e => setcontinentValue(e.target.value)} >
+                            <option disabled value="Select continent">Select continent</option>
+                            {continents.map(continent => <option value={continent.name} key={continent.id}>{continent.name}</option>)}
+                        </select>
+                        </div>
+                        <img src={selectorArrow} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <h6>Country Name:</h6>
+                        <div className="input">
+                        <div>
+                        <img src={search} alt="" />
+                        <select defaultValue={"select a country"}   onChange={e => setcountriesValue(e.target.value)} disabled={disableCountrie}>
+                            {
+                                continentValue ? <>
+                                <option value="select a country" disabled>select a country</option>
+                                {countries[continentValue].map(continent => <option value={continent} key={continent}>{continent}</option>) }</>
+                                : <option>Country</option>
+                            }
+                        </select>
+                        </div>
+                        <img src={selectorArrow} alt="" />
         
-        <img src={map} className="map-image" alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <h6>Region Name:</h6>
+                        <div className="input">
+                        
+                                {
+                                    loadingRegion ? <LittleLoading/> :
+                                    <>
+                                    <div>
+                        <img src={search} alt="" />
+                        <select defaultValue={"select a region"}   onChange={e => setRegionValue(e.target.value)} disabled={disableregion } >
+                            <option value="select a region" disabled>select a region</option>
+                            {region.map(continent => <option value={continent.name} key={continent.id}>{continent.name}</option>)}
+                        </select>
+                        </div>
+                        <img src={selectorArrow} alt="" />
+                                    </>
+                                }
+                        </div>
+                    </div>
+                </div>
+                <img src={cities} className="map-image" alt="" />
+                </>
+                :
+                <Loading/>
+        }
+
+        
+        
         
     </main>
     <img src={message} alt=""  className="message"/>
