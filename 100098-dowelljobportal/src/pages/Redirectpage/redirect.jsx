af71ed6db3186667e4b2f05bfacd203325e8823e
Blackbullet99 <isaacokanigbe@gmail.com>
2023-02-23 11:43:38 -0800

    fixed the session id redirect url and made it dynamic

--- 100098-dowelljobportal/src/pages/Redirectpage/redirect.jsx
+++ 100098-dowelljobportal/src/pages/Redirectpage/redirect.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+
 import Socialmedia2 from "./assets/Socialmedia2.png";
 import "./redirect.css";
 
 const RedirectPage = () => {
+  const [sessionId, setSessionId] = useState(null);
+
+  useEffect(() => {
+    //get session Id from session storage
+    const sessionId = sessionStorage.getItem("session_id");
+    if (sessionId) {
+      setSessionId(sessionId);
+    }
+  }, []);
+
+  //update session Id in session storage
+  useEffect(() => {
+    if (sessionId) {
+      sessionStorage.setItem("session_id", sessionId);
+    } else {
+      sessionStorage.removeItem("session_id");
+    }
+  }, []);
+
   const handleRedirectClick = () => {
-    window.location.href =
-      "https://100093.pythonanywhere.com/?session_id=jghx23fnfb0pkkj8lan5yppe4p6o1qvl";
+    window.location.replace(
+      `https://100093.pythonanywhere.com/?session_id=${sessionId}`
+    );
   };
   return (
     <>
@@ -12,7 +34,9 @@ const RedirectPage = () => {
         <img src={Socialmedia2} alt="Socialmedia2" />
         <p className="redirect">
           You Don't Have a Portfolio,{" "}
-          <span onClick={handleRedirectClick} className="handle-redirect">Click Here</span>
+          <span onClick={handleRedirectClick} className="handle-redirect">
+            Click Here
+          </span>
         </p>
       </div>
     </>
