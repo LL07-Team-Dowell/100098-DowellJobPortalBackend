ce5d01554c1068f73670237ea9be505dde9445ba
alaasassii <alaasassijs@gmail.com>
2023-03-24 06:25:43 +0100

    message

--- 100098-dowelljobportal/src/pages/HrPage/views/Tasks/HrTasks.jsx
+++ 100098-dowelljobportal/src/pages/HrPage/views/Tasks/HrTasks.jsx
@@ -7,41 +7,51 @@ import TitleNavigationBar from '../../../../components/TitleNavigationBar/TitleN
 import AssignedProjectDetails from '../../../../pages/TeamleadPage/components/AssignedProjectDetails/AssignedProjectDetails.js' ;
 import TaskScreen from '../../../TeamleadPage/views/TaskScreen/TaskScreen.js';
 import {useSearchParams} from 'react-router-dom'
+import { fetchCandidateTasks } from '../../../../services/commonServices'; 
+import './index.scss'
+import { differenceInCalendarDays } from 'date-fns';
+
 const HrTasks = () => {
+            // searchParams
             let [searchParams, setSearchParams] = useSearchParams();
-            const applicant = searchParams.get('applicant')
+            const applicant = searchParams.get('applicant') ; 
+            
+            // states
             const [data ,setdata] = useState(testTasksToWorkWithForNow.filter(s => s.applicant === applicant)) ; 
-            useEffect(()=>{
-              setdata(testTasksToWorkWithForNow.filter(s => s.applicant === applicant)) 
-            },[applicant])
-            console.log("data" , data)
             const [project , setproject] = useState("") ;
             const [taskdetail , settaskdetail] = useState([]) ; 
             const [taskdetail2 , settaskdetail2] = useState([]) ; 
             const [value, onChange] = useState(new Date());
-
-            console.log("applicant",applicant) ;
-            console.log(value.toString(), "value")
-            // loading 
+            const [ datesToStyle, setDatesToStyle ] = useState([]);
+            const [noApplicant , setnoApplicant] = useState(false) ; 
+            // List 
+            const List = Array.from(new Set(data.map(d => d.project))) ; 
             useEffect(()=>{
-                        settaskdetail(data.filter(d => d.project === project))
-            },[project , data ]);
+              if(data.length === 0  ){
+                  setnoApplicant(true) ;
+              }
+            },[applicant , data]) ; 
+            
+            useEffect(()=>{
+              setdata(testTasksToWorkWithForNow.filter(s => s.applicant === applicant)) 
+            },[applicant])
+
+            useEffect(()=>{
+                        const newdata = data.filter(d => d.project === project) ; 
+                        settaskdetail(newdata) ;
+                        const datesUserHasTask = [...new Set(data.map(task => [ new Date(task.task_created_date) ])).values()].flat();
+                        console.log({datesUserHasTask})
+                        setDatesToStyle(datesUserHasTask) ; 
+                      },[project , data ]);
+
             useEffect(()=>{
                         settaskdetail2(taskdetail.filter(d => {
                         const dateTime = d.task_created_date.split(" ")[0]+ " " + d.task_created_date.split(" ")[1]+ " " + d.task_created_date.split(" ")[2]+ " " + d.task_created_date.split(" ")[3] ;
                         const calendatTime = value.toString().split(" ")[0] + " " + value.toString().split(" ")[1] + " " + value.toString().split(" ")[2] + " " +value.toString().split(" ")[3] 
                         return dateTime === calendatTime ; 
             }))
-            console.log({data},{data2:data.filter(d => {
-              const dateTime = d.task_created_date.split(" ")[0]+ " " + d.task_created_date.split(" ")[1]+ " " + d.task_created_date.split(" ")[2]+ " " + d.task_created_date.split(" ")[3] ;
-              const calendatTime = value.toString().split(" ")[0] + " " + value.toString().split(" ")[1] + " " + value.toString().split(" ")[2] + " " +value.toString().split(" ")[3] 
-              return dateTime === calendatTime ; 
-  })})
             },[value , taskdetail , data]);
-            console.log("dataaa",Array.from(new Set(data)))
-            console.log("project",project) ;
-            const List = Array.from(new Set(data.map(d => d.project))) ; 
-            console.log("LIST",List)
+
             function getFullMonthName(dateString) {
               const date = new Date(dateString);
               const monthIndex = date.getMonth();
@@ -52,28 +62,43 @@ const HrTasks = () => {
               const fullMonthName = months[monthIndex];
               return fullMonthName;
             }
-            console.log({data ,taskdetail , taskdetail2 })
+
+            const isSameDay = (a, b) => differenceInCalendarDays(a, b) === 0;
+
+            const tileClassName = ({ date, view }) => {
+                // Add class to tiles in month view only
+                if (view === 'month') {
+                    // Check if a date React-Calendar wants to check is on the list of dates to add class to
+                    if (datesToStyle.find(dDate => isSameDay(dDate, date))) {
+                        return 'task__Indicator';
+                    }
+                }
+            }
   return (
     <StaffJobLandingLayout hrView={true}>
+           {
+            noApplicant ? <>NO Applicant</> 
+            :
+            <>
             <div>
-            <div>
+            <div style={{marginTop:40}}>
             <TitleNavigationBar title={"Task details"}/>
             <AssignedProjectDetails showTask={true} hrAttendancePageActive={false} availableProjects={List} removeDropDownIcon={false} handleSelectionClick={e => setproject(e)} />
-            {
-                        // taskdetail.map((d,i)=><li key={i}>{d.task}</li>)
-            }
+            
             </div>
            
 
             </div>
-            <div style={{display:"flex" ,gap:"2rem" }}>
-             <Calendar onChange={onChange} value={value} />
+            <div style={{display:"flex" ,gap:"2rem", marginLeft:100 ,  }}>
+             <Calendar onChange={onChange} value={value}  tileClassName={tileClassName}  />
              <div style={{}}>
               <h4>{getFullMonthName(value.toString())}</h4>
              <ul>{taskdetail2.length > 0 ? taskdetail2.map((d , i) => <li style={{listStyle:"none",color:"#ccc",fontWeight:400}} key={i}>{d.task}</li>) : "No Tasks Found For Today"}</ul>
 
              </div>
              </div>
+             </>
+           }
     </StaffJobLandingLayout>
   )
 }
@@ -85,3 +110,6 @@ export default HrTasks
 
                         // console.log(dateTime , "dateTime") ;
                         // console.log(calendatTime , "calendatTime")
+                        {
+                          // taskdetail.map((d,i)=><li key={i}>{d.task}</li>)
+              }
