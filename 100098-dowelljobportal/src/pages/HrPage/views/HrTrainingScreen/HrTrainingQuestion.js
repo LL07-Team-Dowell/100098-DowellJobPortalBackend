c8c93c0650e2dbed3b6e6ba54d7d4505cdeac997
Blackbullet99 <isaacokanigbe@gmail.com>
2023-05-04 21:43:14 -0700

    fixed design on trainingQuestion page and responsiveness

--- 100098-dowelljobportal/src/pages/HrPage/views/HrTrainingScreen/HrTrainingQuestion.js
+++ 100098-dowelljobportal/src/pages/HrPage/views/HrTrainingScreen/HrTrainingQuestion.js
@@ -3,14 +3,15 @@ import { useNavigate, useParams } from "react-router-dom";
 import { MdArrowBackIosNew } from "react-icons/md";
 import { useCurrentUserContext } from "../../../../contexts/CurrentUserContext";
 import { createQuestionForTrainingMangement } from "../../../../services/hrTrainingServices";
-import { NavigationContextProvider } from "../../../../contexts/NavigationContext";
+import "./Hr_TrainingQuestion.css";
+import { toast } from "react-toastify";
 
 function HrTrainingQuestions({ trainingCards }) {
   const { currentUser } = useCurrentUserContext();
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
-  const { module } = useParams();
+  // const { module } = useParams();
 
   const [questions, setQuestions] = useState({
     company_id: currentUser.portfolio_info[0].org_id,
@@ -22,32 +23,80 @@ function HrTrainingQuestions({ trainingCards }) {
     is_active: true,
   });
 
-  useEffect(() => {
-    createQuestionForTrainingMangement(questions);
-  }).then((res) => console.log(res, "res"));
+  const handleOnChange = (valueEntered, inputName) => {
+    setQuestions((prevValue) => {
+      const copyOfPrevValue = { ...prevValue };
+      copyOfPrevValue[inputName] = valueEntered;
+      return copyOfPrevValue;
+    });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(questions);
+
+    // const fields = ["question_link"];
+
+    // if (questions.question_link === "") {
+    //   toast.info("Please input question link");
+    //   return;
+    // } else if (fields.find((field) => questions[field] === "")) {
+    //   toast.info(
+    //     `Please input ${fields.find((field) => questions[field] === "")} field`
+    //   );
+    //   return;
+    // }
+
+    setIsLoading(true);
+    try {
+      const response = await createQuestionForTrainingMangement(questions);
+      console.log(response.data);
+
+      // if (response.status === 201) {
+      //   setQuestions((prevValue) => [questions, ...prevValue]);
+      //   toast.success("Question created successfully");
+      //   navigate("/hr-training");
+      // } else {
+      //   toast.error("Question failed to be created");
+      // }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+
+    setIsLoading(false);
+  };
 
   return (
     <>
       <div className="container">
-        <div>
-          <button onClick={() => navigate(-1)} style={{ position: "relative" }}>
-            <MdArrowBackIosNew
-              style={{
-                color: "#005734",
-                position: "absolute",
-                fontSize: 25,
-                top: "20%",
-                left: "21%",
-              }}
-            />
-          </button>
-          {trainingCards
-            .filter((card) => card.module === module)
-            .map((card) => (
-              <div key={card.id}>
-                <h1>{card.module}</h1>
+        <div className="question__background">
+          <div className="question__description">
+            <div className="question__top__background"></div>
+            <div className="question__body">
+              <div className="head">
+                <h2 className="question__title">Add Form Title</h2>
+                <span></span>
               </div>
-            ))}
+              <input
+                type="text"
+                name={"question_link"}
+                value={questions.question_link}
+                placeholder="Add Description"
+                className="question__link"
+                onChange={(e) => handleOnChange(e.target.value, e.target.name)}
+                required
+              />
+              <div className="bottom">
+                <button
+                  className="send__btn"
+                  onClick={(e) => handleOnSubmit(e)}
+                >
+                  Send
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
