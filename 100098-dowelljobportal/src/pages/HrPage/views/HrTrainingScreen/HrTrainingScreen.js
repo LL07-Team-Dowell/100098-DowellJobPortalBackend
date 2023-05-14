62625c4d99682807be4163b77d3937542851f86a
Blackbullet99 <isaacokanigbe@gmail.com>
2023-05-13 21:45:16 -0700

    finished design and implementation of apis on hrTrainingScreen page and also implemented a new feature on admin page

--- 100098-dowelljobportal/src/pages/HrPage/views/HrTrainingScreen/HrTrainingScreen.js
+++ 100098-dowelljobportal/src/pages/HrPage/views/HrTrainingScreen/HrTrainingScreen.js
@@ -4,26 +4,33 @@ import { Link } from "react-router-dom";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { getTrainingManagementQuestions } from "../../../../services/hrTrainingServices";
 import { useHrJobScreenAllTasksContext } from "../../../../contexts/HrJobScreenAllTasks";
+import { useCurrentUserContext } from "../../../../contexts/CurrentUserContext";
 
-function HrTrainingScreen({ trainingCards }) {
+function HrTrainingScreen({ trainingCards, setShowOverlay }) {
   const { questions, setQuestions } = useHrJobScreenAllTasksContext();
 
-  useEffect(() => {
-    getTrainingManagementQuestions({
-      document_id: crypto.randomUUID(),
-    })
-      .then((res) => {
-        setQuestions(res.data);
-        console.log(res.data);
+  const { currentUser } = useCurrentUserContext();
 
-        if (res.data.length === 0) {
-          console.log("No data");
-        } else if (res.data.length > 0) {
-          console.log("Data is present");
-        }
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  const fetchCreatedQuestions = async (e) => {
+    e.preventDefault();
+
+    setShowOverlay(true);
+
+    try {
+      const response = await getTrainingManagementQuestions({
+        company_id: currentUser.portfolio_info[0].org_id,
+      });
+
+      const ListOfQuestions = response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClick = (e, link) => {
+    e.preventDefault();
+    window.open(link, "_blank");
+  };
 
   return (
     <div className="training__wrapper">
@@ -34,16 +41,53 @@ function HrTrainingScreen({ trainingCards }) {
         {trainingCards.map((card) => (
           <div className="training__cards" key={card.id}>
             <div className="svg_component">{card.svg}</div>
+            {questions.find((question) => question.module === card.module)
+              ?.newly_created ? (
+              <div className="edit">
+                <Link
+                  to={`/hr-training/${card.module}`}
+                  className="edit__btn"
+                  onClick={fetchCreatedQuestions}
+                >
+                  {card.action}
+                </Link>
+              </div>
+            ) : (
+              <></>
+            )}
             <div className="training__card__body">
               <h2>{card.module}</h2>
               <p className="training__card__description">{card.description}</p>
             </div>
-            <Link to={`/hr-training/${card.module}`} style={{ color: "black" }}>
-              <button className="action__btn">
-                <span>Create Now</span>
-                <AiOutlineArrowRight fontSize="1.4rem" />
-              </button>
-            </Link>
+            {questions.find((question) => question.module === card.module) ? (
+              <Link
+                to={`/hr-training/#`}
+                style={{ color: "black" }}
+                onClick={(e) =>
+                  handleClick(
+                    e,
+                    questions.find(
+                      (question) => question.module === card.module
+                    ).question_link
+                  )
+                }
+              >
+                <button className="action__btn">
+                  <span>View Now</span>
+                  <AiOutlineArrowRight fontSize="1.4rem" />
+                </button>
+              </Link>
+            ) : (
+              <Link
+                to={`/hr-training/${card.module}`}
+                style={{ color: "black" }}
+              >
+                <button className="action__btn">
+                  <span>Create Now</span>
+                  <AiOutlineArrowRight fontSize="1.4rem" />
+                </button>
+              </Link>
+            )}
           </div>
         ))}
       </div>
