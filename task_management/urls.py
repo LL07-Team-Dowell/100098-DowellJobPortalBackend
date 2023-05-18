4e44cc80b4af34a6eb47607d1a7526621104cc1c
Manish <manish@dowellresearch.in>
2023-05-19 01:19:58 +0530

    fixed task management api url

--- task_management/urls.py
+++ task_management/urls.py
@@ -4,7 +4,7 @@ from task_management.task_views import *
 urlpatterns = [
     path('create_task/', create_task.as_view()),
     path('get_task/<str:company_id>/', get_task.as_view()),
-    path('get_candidate_task/<str:document_id/', get_cadidate_task.as_view()),
+    path('get_candidate_task/<str:document_id>/', get_candidate_task.as_view()),
     path('update_task/', update_task.as_view()),
     path('delete_task/', delete_task.as_view()),
 ]
