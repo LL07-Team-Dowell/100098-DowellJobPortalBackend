67ca75d65dda5e82f6288692a5e6fec336d4e6ba
Blackbullet99 <isaacokanigbe@gmail.com>
2023-06-09 16:06:47 -0700

    added active status to teamlead and account page and also removed the edit button

--- src/pages/TeamleadPage/views/UserScreen/UserScreen.js
+++ src/pages/TeamleadPage/views/UserScreen/UserScreen.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useCurrentUserContext } from "../../../../contexts/CurrentUserContext";
 import {getUserLiveStatus , postUserLiveStatus } from "../../../../services/commonServices" ; 
 import "./style.css";
 import { FiberPinRounded } from "@mui/icons-material";
+import { teamManagementProductName } from "../../../../utils/utils";
 
 
 const UserScreen = () => {
     const { currentUser } = useCurrentUserContext();
+    const [success ,setsuccsess] = useState(false);
 
     console.log({currentUser}) ;
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkActive = setInterval(()=>{
+            Promise.all([
+                getUserLiveStatus(),
+                postUserLiveStatus({product: teamManagementProductName, session_id:sessionStorage.getItem("session_id")})])
+                .then(resp => {
+                    console.log(resp[0],resp[1]);
+                    if(resp[0]){
+                        setsuccsess(true) ; 
+                    }
+                }) 
+                .catch(err => {console.log(err[0],err[1]);setsuccsess(false);}); 
+            },60000)
+            return () => clearInterval(checkActive)
+    })
 
     const handleLogout = () => navigate("/logout");
     // React.useEffect(()=>{
@@ -26,9 +45,6 @@ const UserScreen = () => {
                     <h2>User Name</h2>
                     <span>{ currentUser?.userinfo.username }</span>    
                 </div>
-                <div className="edit__Btn">
-                    Edit
-                </div>
             </div>
             <div className="user__Intro__Item">
                 <h2>Email</h2>
@@ -36,27 +52,23 @@ const UserScreen = () => {
             </div>
             <div className="user__Intro__Item">
                 <h2>First Name </h2>
-                <span>{currentUser.first_name}</span>
+                <span>{currentUser?.userinfo.first_name}</span>
             </div>
             {
                 currentUser.last_name !== "" &&
                 <div className="user__Intro__Item">
                     <h2>Last Name</h2>
-                    <span>{currentUser.last_name}</span>
+                    <span>{currentUser?.userinfo.last_name}</span>
                 </div>
             }
             <div className="user__Intro__Item">
                 <h2>Role</h2>
                 <span>TeamLead</span>
             </div>
-            <div className="user__Intro__Item">
-                <h2>Role</h2>
-                <span>TeamLead</span>
-            </div>
-            {/* <div className="user__Intro__Item" style={{display:"flex",gap:5,alignItems:"center"}}>
+            <div className="user__Intro__Item" style={{display:"flex",gap:5,alignItems:"center"}}>
                 <h2>Active Status</h2>
-                <div style={successStatus}></div>
-            </div> */}
+                <div style={success ? successStatus : failedStatus}></div>
+            </div> 
             <div className="user__Intro__Item">
                 <h2>Project</h2>
                 <span>{currentUser.settings_for_profile_info.profile_info[0]?.project}</span>
