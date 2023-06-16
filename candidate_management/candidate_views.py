ee6454c2fbc7ff9f6523b28031b0f19262e6844d
Oscar <oscarchiagoziem@gmail.com>
2023-06-16 13:34:55 +0100

    corrected the apply duplicate api

--- candidate_management/candidate_views.py
+++ candidate_management/candidate_views.py
@@ -13,11 +13,12 @@ from candidate_management.serializers import CandidateSerializer
 import datetime
 from dateutil.relativedelta import relativedelta
 
+
 # apply for jobs
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class ApplyJob(APIView):
+class apply_job(APIView):
     def is_eligible_to_apply(self, applicant_email):
         data = self.request.data
         field = {
@@ -28,21 +29,26 @@ class ApplyJob(APIView):
         update_field = {
             "status": "nothing to update"
         }
-        applicant = dowellconnection(*rejected_reports_modules, "fetch", field, update_field)
-        rejected_reports_modules = []
-        rejected_reports_modules.append(applicant)
-        
+        applicant = dowellconnection(*hr_management_reports, "fetch", field, update_field)
+        # rejected_reports_modules = []
+        # rejected_reports_modules.append(applicant)
+
         # Check if applicant is present in rejected_reports_modules
         if applicant is not None:
-            rejected_on = applicant.get("rejected_on")
+            rejected_dates = [datetime.datetime.strptime(item["rejected_on"], '%m/%d/%Y') for item in
+                              json.loads(applicant)["data"]]
+            rejected_on = max(rejected_dates)  # get last date
+            # print(rejected_on, "=======================")
             if rejected_on:
                 three_months_after = rejected_on + relativedelta(months=3)
-                current_date = datetime.timezone.now().date()
+                #print(rejected_on, "=======================", three_months_after)
+                current_date = datetime.datetime.today()
+                print(rejected_on, "==========", three_months_after,"=========",current_date)
                 if current_date >= three_months_after:
                     return True
 
         return False
-    
+
     def post(self, request):
         data = request.data
         applicant_email = data.get("applicant_email")
@@ -92,64 +98,9 @@ class ApplyJob(APIView):
         if serializer.is_valid():
             response = dowellconnection(*rejected_reports_modules, "fetch", field, update_field)
             if response:
-                return Response({"message": "Application received."}, status=status.HTTP_201_CREATED)
-            else:
-                return Response({"message": "Application failed to receive."}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            default_errors = serializer.errors
-            new_error = {}
-            for field_name, field_errors in default_errors.items():
-                new_error[field_name] = field_errors[0]
-            return Response(new_error, status=status.HTTP_400_BAD_REQUEST)
-    
-@method_decorator(csrf_exempt, name='dispatch')
-class apply_job(APIView):
-    def post(self, request):
-        data = request.data
-        field = {
-            "eventId": get_event_id()['event_id'],
-            "job_number": data.get('job_number'),
-            "job_title": data.get('job_title'),
-            "applicant": data.get('applicant'),
-            "applicant_email": data.get('applicant_email'),
-            "feedBack": data.get('feedBack'),
-            "freelancePlatform": data.get('freelancePlatform'),
-            "freelancePlatformUrl": data.get('freelancePlatformUrl'),
-            "academic_qualification_type": data.get('academic_qualification_type'),
-            "academic_qualification": data.get('academic_qualification'),
-            "country": data.get('country'),
-            "job_category": data.get('job_category'),
-            "agree_to_all_terms": data.get('agree_to_all_terms'),
-            "internet_speed": data.get('internet_speed'),
-            "other_info": data.get('other_info'),
-            "project": "",
-            "status": "Pending",
-            "hr_remarks": "",
-            "teamlead_remarks": "",
-            "rehire_remarks": "",
-            "module": data.get("module"),
-            "server_discord_link": "https://discord.gg/Qfw7nraNPS",
-            "product_discord_link": "",
-            "payment": data.get('payment'),
-            "company_id": data.get('company_id'),
-            "username": data.get('username'),
-            "portfolio_name": data.get('portfolio_name'),
-            "data_type": data.get('data_type'),
-            "scheduled_interview_date": "",
-            "application_submitted_on": data.get('application_submitted_on'),
-            "shortlisted_on": "",
-            "selected_on": "",
-            "hired_on": "",
-            "onboarded_on": "",
-        }
-        update_field = {
-            "status": "nothing to update"
-        }
-        serializer = CandidateSerializer(data=field)
-        if serializer.is_valid():
-            response = dowellconnection(*candidate_management_reports, "insert", field, update_field)
-            if response:
-                return Response({"message": "Application received."}, status=status.HTTP_201_CREATED)
+                return Response({"message": "Application received.",
+                                 "Eligibility": self.is_eligible_to_apply(applicant_email)
+                                 }, status=status.HTTP_201_CREATED)
             else:
                 return Response({"message": "Application failed to receive."}, status=status.HTTP_400_BAD_REQUEST)
         else:
@@ -198,6 +149,7 @@ class get_all_onboarded_candidate(APIView):
             return Response({"message": "There are no onboarded Candidates", "response": json.loads(response)},
                             status=status.HTTP_204_NO_CONTENT)
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class get_candidate_application(APIView):
 
@@ -216,7 +168,8 @@ class get_candidate_application(APIView):
                             status=status.HTTP_200_OK)
         else:
             return Response({"message": "There is no job applications", "response": json.loads(response)},
-                                status=status.HTTP_204_NO_CONTENT)
+                            status=status.HTTP_204_NO_CONTENT)
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class delete_candidate_application(APIView):
