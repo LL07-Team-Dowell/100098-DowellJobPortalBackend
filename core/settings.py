cc4b435e95de50f6ec5cbea41d7308ffea3fbf73
hardik <c.hardik125@gmail.com>
2023-06-20 14:05:26 +0530

    setting changes

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.utils.translation',
     'app',
     "corsheaders",
     "rest_framework",
