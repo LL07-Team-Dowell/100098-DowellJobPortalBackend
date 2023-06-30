535645683cfb09a73a71257bcc6c64770a60183f
alaasassii <alaasassijs@gmail.com>
2023-06-30 09:00:57 +0100

    message

--- src/pages/TeamleadPage/views/CreateMembersTask/component/Teams.jsx
+++ src/pages/TeamleadPage/views/CreateMembersTask/component/Teams.jsx
@@ -6,6 +6,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 // Fetch Teams for that company 
 import { teams ,imageReturn} from '../assets/teamsName';
 import { useNavigate } from 'react-router-dom';
+import { AiOutlineTeam } from 'react-icons/ai';
 const Teams = ({back , setChoosedTeam}) => {
   const { currentUser } = useCurrentUserContext();
     console.log(currentUser.portfolio_info[0].org_id)
@@ -26,7 +27,7 @@ const Team = ({v,team_name ,setChoosedTeam}) => {
   
   return (
     <li className='team' onClick={()=>{navigate(`/team-screen-member/${v._id}/team-tasks`)}}>
-      <img src={imageReturn(team_name) !== null ? imageReturn(team_name): null}  />
+    {imageReturn(team_name) ?  <img src={imageReturn(team_name)} /> : <AiOutlineTeam style={{width:86,height:86,backgroundColor:'rgba(225, 251, 226, 1)',color:'rgba(0, 87, 52, 1)',borderRadius:'50%',fontSize:10,fontWeight:600,padding:10,boxSizing:'border-box'}}/>}
       <h2>{team_name}</h2>
       <p className='paragraph-discription'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur sapiente nostrum quibusdam eum odit animi dolorem iusto earum non? </p>
       <button>View More <HiArrowNarrowRight/></button>
