c70008962e0212288a490db9624f73eed78f2a06
Shaunak KD <sagardeyyash19@gmail.com>
2023-07-06 09:29:44 +0600

    correction on applied job page

--- src/pages/CandidatePage/components/AppliedScreen/Applied.js
+++ src/pages/CandidatePage/components/AppliedScreen/Applied.js
@@ -9,11 +9,13 @@ import LoadingSpinner from '../../../../components/LoadingSpinner/LoadingSpinner
 import { useCandidateJobsContext } from '../../../../contexts/CandidateJobsContext';
 import { getAppliedJobs, getJobs } from '../../../../services/candidateServices';
 import { useCurrentUserContext } from '../../../../contexts/CurrentUserContext';
+import { IoMdRefresh } from 'react-icons/io';
 
 
 function Applied() {
   const [currentNavigationTab, setCurrentNavigationTab] = useState("Applied");
   const { candidateJobs, setCandidateJobs } = useCandidateJobsContext();
+  console.log(candidateJobs);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { currentUser } = useCurrentUserContext();
@@ -45,7 +47,7 @@ function Applied() {
         console.log(currentUserAppliedJobs);
         setCandidateJobs((prevJobs) => { return { ...prevJobs, "appliedJobs": currentUserAppliedJobs } });
         setCandidateJobs((prevJobs) => { return { ...prevJobs, "currentUserApplications": currentUserApplications } });
-        setCandidateJobs((prevJobs) => { return { ...prevJobs, "userInterviews": currentUserApplications.filter(application => application.status === candidateStatuses.PENDING_SELECTION) } })
+        setCandidateJobs((prevJobs) => { return { ...prevJobs, "userInterviews": currentUserApplications.filter(application => application.status === candidateStatuses.SHORTLISTED) } })
         return setLoading(false);
 
       } catch (error) {
@@ -60,9 +62,60 @@ function Applied() {
 
   }, [])
 
+  const handleRefreshForCandidateApplications = () => {
+    // if (candidateJobs.appliedJobs.length > 0) return setLoading(false);
+    setLoading(true);
+    const datass = currentUser.portfolio_info[0].org_id;
+    getAppliedJobs(datass).then(async (res) => {
+      const userApplication = res.data.response.data.filter(
+        (application) => application.data_type === currentUser?.portfolio_info[0].data_type
+      );
+      // console.log(userApplication);
+      try {
+        const jobs = await (await getJobs(datass));
+        // console.log(jobs);
+        const userAppliedJobs = jobs.data.response.data.filter(
+          (currentJob) => currentJob.data_type === currentUser?.portfolio_info[0].data_type
+        )
+
+        // if (Array.isArray(candidateJobs.appliedJobs) && candidateJobs.appliedJobs.length > 1) return setLoading(false);
+        const currentUserApplications = userApplication.filter(
+          (application) =>
+            application.username === currentUser.userinfo.username
+        );
+        const currentUserAppliedJobs = userAppliedJobs.filter((currentJob) =>
+          currentUserApplications.find(
+            ({ job_number }) => currentJob.job_number === job_number
+          )
+        );
+        console.log(currentUserAppliedJobs);
+        const sortJob = currentUserAppliedJobs.sort((a, b) => new Date(b.created_on) - new Date(a.created_on));
+        setCandidateJobs((prevJobs) => { return { ...prevJobs, "appliedJobs": sortJob } });
+        setCandidateJobs((prevJobs) => { return { ...prevJobs, "currentUserApplications": currentUserApplications } });
+        setCandidateJobs((prevJobs) => { return { ...prevJobs, "userInterviews": currentUserApplications.filter(application => application.status === candidateStatuses.PENDING_SELECTION) } })
+        return setLoading(false);
+
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+
+    }).catch(err => {
+      console.log(err);
+      setLoading(false);
+    })
+
+  }
+
+
   return <>
+
     <TogglerNavMenuBar className={"applied__Nav__Toggler"} menuItems={["Applied", "Interview", "Declined"]} handleMenuItemClick={(item) => setCurrentNavigationTab(item)} currentActiveItem={currentNavigationTab} />
+    <div className="refresh-container" id='refresh-container' onClick={handleRefreshForCandidateApplications}>
+      <IoMdRefresh />
+    </div>
     <div className="candidate__View__Applications__Container">
+
       {
         loading ? <LoadingSpinner /> :
 
@@ -75,18 +128,18 @@ function Applied() {
                   buttonText={"View"}
                   candidateData={application}
                   handleBtnClick={
-                    candidateJobs.appliedJobs.find(job => job.job_number === application.job_number) ? 
+                    candidateJobs.appliedJobs.find(job => job.job_number === application.job_number) ?
                       () => navigate(
-                        "/applied/view_job_application", 
-                        { 
-                          state: { 
-                            jobToView: candidateJobs.appliedJobs.find(job => job.job_number === application.job_number), 
+                        "/applied/view_job_application",
+                        {
+                          state: {
+                            jobToView: candidateJobs.appliedJobs.find(job => job.job_number === application.job_number),
                             applicationDetails: application
-                          } 
+                          }
                         }
                       )
-                    :
-                    () => {}
+                      :
+                      () => { }
                   }
                 />
               }))
