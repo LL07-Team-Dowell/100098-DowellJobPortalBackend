86af6965f7aeebf5a6553d67e4dc8b0af97286cd
alaasassii <alaasassijs@gmail.com>
2023-07-12 05:49:25 +0100

    updating team_task

--- src/pages/TeamleadPage/views/CreateMembersTask/views/TeamScreenMembers.jsx
+++ src/pages/TeamleadPage/views/CreateMembersTask/views/TeamScreenMembers.jsx
@@ -41,7 +41,7 @@ const TeamScreenMembers = () => {
       <Navbar title={team?.team_name} removeButton={true}/>
       <TeamScreenLinks id={id}/>
       <TeamScreenMembersContainer members={team?.members}/>
-      {showPopup && <AddMemberPopup bigMember={bigMember} members={team?.members} setmembers={setmembers} setTeamName={setTeamName} team_name={team?.team_name} close={()=>setShowPopup(false)} getElementToTeamState={getElementToTeamState} team={team}/>}
+      {showPopup && <AddMemberPopup bigMember={bigMember} members={team?.members} setmembers={setmembers} setTeamName={setTeamName} team_name={team?.team_name} close={()=>setShowPopup(false)} getElementToTeamState={getElementToTeamState} team={team} setteam={setteam}/>}
     <button className='add-member-btn' onClick={()=>setShowPopup(true)}><div><HiOutlinePlusSm fontWeight={700}/> <span>add member</span></div></button> 
     </div>
   )
--- src/pages/TeamleadPage/views/CreateMembersTask/views/compoonent/addMemberPopup/addMemberPopup.jsx
+++ src/pages/TeamleadPage/views/CreateMembersTask/views/compoonent/addMemberPopup/addMemberPopup.jsx
@@ -3,7 +3,8 @@ import './addMemberPopup.scss'
 import { useCurrentUserContext } from '../../../../../../../contexts/CurrentUserContext';
 import { EditTeam } from '../../../../../../../services/createMembersTasks';
 import { toast } from 'react-toastify';
-
+import {BsPlus} from 'react-icons/bs'
+import {FaTimes} from 'react-icons/fa'
 const returnMissingMember = (bigMember,smallMember) => {
     let data = bigMember
     smallMember.forEach(element => {
@@ -12,21 +13,34 @@ const returnMissingMember = (bigMember,smallMember) => {
     return data
 }
 
-const AddMemberPopup = ({bigMember,  members,team_name, setmembers ,close, setTeamName,getElementToTeamState}) => {
+
+const AddMemberPopup = ({bigMember,  members,team_name, setmembers ,close, setTeamName,getElementToTeamState, setteam, team}) => {
   const { currentUser } = useCurrentUserContext();
     const [addedMembers , setAddedMembers] = useState(members); 
     const [name, setname] = useState(team_name) ;
     const userIsThere = (user) => addedMembers.includes(user);
     const addMembers = (member) => setAddedMembers(p => [...p,member]) ; 
-  const removeMember = (member) => setAddedMembers(p => p.filter(filteredMember => filteredMember !== member )) ; 
-    
-       
+  // const removeMember = (member) => setAddedMembers(p => p.filter(filteredMember => filteredMember !== member )) ; 
+  const [displaidMembers, setDesplaidMembers] = useState(bigMember.map((member,index) => ({id:index ,member})))
+  const [inputMembers, setInputMembers] = useState([]) ; 
+  const [query,setquery] = useState('');
+  const AddedMember = (id) => {
+    setInputMembers([...inputMembers,displaidMembers.find(f => f.id === id)])
+    setDesplaidMembers(displaidMembers.filter(f => f.id !== id))
+  }
+  const removeMember = (id) => {
+    console.log({newinputMembers:inputMembers.filter(f => f.id !== id), newDesplaidMembers:[...displaidMembers,displaidMembers.find(f => f.id === id)] })
+    setInputMembers(inputMembers.filter(f => f.id !== id))
+    setDesplaidMembers([...displaidMembers,inputMembers.find(f => f.id === id)])
+  }
+  console.log({displaidMembers, inputMembers})
       const EditTeamFunction = () => {
-        if(name && addedMembers.length > 0)
-        EditTeam(currentUser.portfolio_info[0].org_id,{team_name,members:[...members ,...addedMembers]})
+        if(name && inputMembers.length > 0)
+        EditTeam(currentUser.portfolio_info[0].org_id,{team_name,members:[...inputMembers.map(m => m.member)]})
           .then(resp => {
             console.log(resp);
-            getElementToTeamState(name,addedMembers)
+            getElementToTeamState(name,inputMembers.map(m => m.member))
+            setteam({...team, members:inputMembers.map(m => m.member)})
             toast.success("Edit Team Successfully");
             close()
           })
@@ -52,19 +66,29 @@ const AddMemberPopup = ({bigMember,  members,team_name, setmembers ,close, setTe
             onChange={(e)=> setname(e.target.value)}
           />
           <br />
-      <label htmlFor='task_name'>Team Name</label>
+          <label htmlFor="">Added Members</label>
+          <div className='added-members-input'>
+            {
+              inputMembers.map(v => <div key={v.id} onClick={()=>removeMember(v.id)}><p>{v.member}</p><FaTimes fontSize={'small'}/></div>)
+            }
+            <input type="text" placeholder='search member' value={query} onChange={e => setquery(e.target.value)}/>
+          </div>
+          <div></div>
+          <br />
+      <label htmlFor='task_name'>Members</label>
       <div className='members'>
-        {bigMember.map((item) => (
-        <div  key={item}>
-            <input
-              type="checkbox"
-              value={item}
-              checked={userIsThere(item)}
-              onChange={()=>{userIsThere(item) ? removeMember(item)  : addMembers(item)}}
-            />
-            <p>{item}</p>
-        </div>
-      ))}
+        {displaidMembers
+          .filter(f => f.member.includes(query)).length > 0 ?
+          displaidMembers
+          .filter(f => f.member.includes(query))
+          .map((element) => (
+          <div className="single-member" onClick={()=>AddedMember(element.id)}>
+            <p>{element.member}</p>
+            <BsPlus/>
+          </div>
+      )):
+      <h3>No More Members</h3>
+      }
       </div>
       <button className='edit-team' onClick={()=>EditTeamFunction()}>Edit Team </button>
     </div>
