eee95ee2a776002a807bec3244ee378dc8fc020b
Blackbullet99 <isaacokanigbe@gmail.com>
2023-07-30 21:23:50 -0700

    fixed modal for adding issues in the candidate page

--- src/pages/TeamleadPage/views/AddIssueScreen/AddIssueScreen.js
+++ src/pages/TeamleadPage/views/AddIssueScreen/AddIssueScreen.js
@@ -10,210 +10,176 @@ import { createCandidateTask } from "../../../../services/candidateServices";
 import { toast } from "react-toastify";
 import { createThread } from "../../../../services/threadServices";
 
+const AddIssueScreen = ({
+  teamMembers,
+  closeIssuesScreen,
+  updateIssues,
+  afterSelectionScreen,
+  editPage,
+  setEditPage,
+  taskToEdit,
+  hrPageActive,
+  assignedProject,
+}) => {
+  const ref = useRef(null);
+  const [showIssueForm, setShowIssueForm] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
+  const { currentUser } = useCurrentUserContext();
+  const [optionValue, setoptionValue] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
 
-const AddIssueScreen = ({ teamMembers, closeTaskScreen, updateTasks, afterSelectionScreen, editPage, setEditPage, taskToEdit, hrPageActive, assignedProject }) => {
+  const [createIssue, setCreateIssue] = useState({
+    thread: "",
+    image: "",
+    team_alearted_id: "",
+    created_by: currentUser.userinfo.username,
+  });
 
-    const ref = useRef(null);
-    const [showIssueForm, setShowIssueForm] = useState(false);
-    const [newTaskDetails, setNewTaskDetails] = useState({
-        "username": "",
-        "title": "",
-        "description": "",
-        "status": "Incomplete",
+  useClickOutside(ref, () => {
+    closeIssuesScreen();
+    !afterSelectionScreen && setEditPage(false);
+  });
+
+  const handleChange = (valueEntered, inputName) => {
+    setCreateIssue((prev) => {
+      const newCreateIssue = { ...prev };
+      newCreateIssue[inputName] = valueEntered;
+      return newCreateIssue;
     });
-    const [disabled, setDisabled] = useState(true);
-    const navigate = useNavigate();
-    const { currentUser } = useCurrentUserContext();
-    const [time, settime] = useState(new Date().toString());
-    const TimeValue = `${time.split(" ")[0]} ${time.split(" ")[1]} ${time.split(" ")[2]} ${time.split(" ")[3]}`
-    const [optionValue, setoptionValue] = useState("");
-    const selctChange = (e) => {
-        setoptionValue(e.target.value);
-    }
-    
+  };
 
-    // console.log(time);
-    useClickOutside(ref, () => { closeTaskScreen(); !afterSelectionScreen && setEditPage(false) });
+  const handleOptionChange = (e) => {
+    setoptionValue(e.target.value);
+    setCreateIssue((prev) => {
+      const newCreateIssue = { ...prev };
+      newCreateIssue["team_alearted_id"] = e.target.value;
+      return newCreateIssue;
+    });
+  };
 
-    // useEffect (() => {
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+    setCreateIssue((prev) => {
+      const newCreateIssue = { ...prev };
+      newCreateIssue["image"] = e.target.files[0];
+      return newCreateIssue;
+    });
+  };
 
-    //     if (newTaskDetails.username.length < 1) return setShowTaskForm(false);
+  useEffect(() => {
+    if (createIssue.thread.length < 1) return setShowIssueForm(false);
+    setShowIssueForm(true);
+  }, [createIssue.thread]);
 
-    //     if ((newTaskDetails.title.length < 1) || (newTaskDetails.description.length < 1)) return setDisabled(true)
+  useEffect(() => {
+    if (createIssue.thread.length < 1) return setDisabled(true);
+    setDisabled(false);
+  }, [createIssue.thread]);
 
-    //     setDisabled(false)
+  const handleCreateIssue = async (e) => {
+    e.preventDefault();
+    // console.log(createIssue);
 
-    // }, [newTaskDetails])
-    useEffect(() => {
-        if (newTaskDetails.description.length < 1 || optionValue.length < 1) return setDisabled(true);
-        setDisabled(false)
-
-    }, [newTaskDetails.description, optionValue]);
-    const CreateNewTaskFunction = () => {
-        setDisabled(true)
-        const dataToPost = {
-            thread: "",
-            image: "",
-            created_by: currentUser.userinfo.username,
-            team_alearted_id: optionValue,
+    try {
+        const response = await createThread(createIssue);
+        console.log(response.data);
+        if (response.status === 201) {
+            toast.success("Issue Created Successfully");
         }
-        createThread(dataToPost)
-          .then((resp) => {
-            console.log(resp);
-            updateTasks((prevTasks) => {
-              return [
-                ...prevTasks,
-                { ...dataToPost, status: newTaskDetails.status },
-              ];
-            });
-            setNewTaskDetails({ ...newTaskDetails, description: "" });
-            setoptionValue("");
-            toast.success("New task sucessfully added");
-            setDisabled(false);
-            closeTaskScreen();
-          })
-          .catch((err) => {
-            console.log(err);
-            setDisabled(false);
-          });
+    } catch (error) {
+        toast.error("Something went wrong");
     }
-    useEffect(() => {
+  };
 
-        if (afterSelectionScreen) {
-            setNewTaskDetails(prevValue => { return { ...prevValue, username: currentUser.userinfo.username } });
-            setShowIssueForm(true);
-        }
-
-    }, [afterSelectionScreen])
-
-    useEffect(() => {
-        if (editPage) {
-
-            setNewTaskDetails({
-                username: taskToEdit.user,
-                title: taskToEdit.title,
-                description: taskToEdit.description,
-            });
-            setShowIssueForm(true);
-
-        }
-    }, [editPage])
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNewTaskDetails(prevValue => { return { ...prevValue, [name]: value } })
-    }
-
-    const handleMemberItemClick = (member) => {
-        setNewTaskDetails(prevValue => { return { ...prevValue, "username": member } });
-        setShowIssueForm(true);
-    }
-
-    const handleNewTaskBtnClick = async () => {
-
-        // setDisabled(true);
-
-        // const dataToSend = { ...newTaskDetails };
-        // dataToSend.user = newTaskDetails.username;
-        // delete dataToSend["username"];
-
-        // try{
-
-        //     const response = await addNewTask(dataToSend);
-
-        //     if (!afterSelectionScreen) updateTasks(prevTasks => { return [ ...prevTasks.filter(task => task.user !== dataToSend.user) ] });
-        //     updateTasks(prevTasks => { return [ response.data, ...prevTasks ] } );
-        //     closeTaskScreen();
-        //     (afterSelectionScreen || hrPageActive) ? navigate("/tasks") : navigate("/task");
-
-        // } catch (err) {
-        //     console.log(err);
-        //     setDisabled(false);
-        // }
-
-    }
-
-    const handleUpdateTaskBtnClick = async () => {
-
-        // setDisabled(true);
-
-        // const dataToSend = { ...newTaskDetails };
-        // dataToSend.user = newTaskDetails.username;
-        // delete dataToSend["username"];
-
-        // try{
-
-        //     await updateSingleTask(taskToEdit.id + "/", dataToSend)
-
-        //     taskToEdit.title = dataToSend.title;
-        //     taskToEdit.description = dataToSend.description;
-
-        //     updateTasks(prevTasks => prevTasks.map(task => {
-
-        //         if (task.id === taskToEdit.id) {
-        //             return { ...task, title: dataToSend.title, description: dataToSend.description }
-        //         }
-
-        //         return task;
-
-        //     }) );
-
-        //     closeTaskScreen();
-        //     navigate("/task");
-
-        // } catch (err) {
-        //     console.log(err);
-        //     setDisabled(false);
-        // }
-
-    }
-
-    return <>
-        <div className="add__New__Task__Overlay">
-            <div className="add__New__Task__Container" ref={ref}>
-                <h1 className="title__Item">
-                    {
-                        showIssueForm ? <>
-                            {!afterSelectionScreen && <IoIosArrowBack onClick={editPage ? () => { closeTaskScreen(); setEditPage(false); } : () => setShowIssueForm(false)} style={{ cursor: "pointer" }} />}
-                            {editPage ? "Edit Issue" : "New Issue Details"}
-                        </> : <>Add new Issue</>
-                    }
-
-                    <AiOutlineClose onClick={() => { closeTaskScreen(); !afterSelectionScreen && setEditPage(false) }} style={{ cursor: "pointer" }} fontSize={'1.2rem'} />
-                </h1>
-                {
-                    showIssueForm ? <>
-                        <span className="selectProject">Enter Issue Details</span>
-                        <textarea placeholder="Enter Issue" name="description" value={newTaskDetails.description} style={{ margin: 0 }} onChange={handleChange} rows={5}></textarea>
-                        <span className="selectProject">Add an Image to help explain your issue better (OPTIONAL)</span>
-                        <input type={"text"} placeholder={"Task Assignee"} value={newTaskDetails.username} style={{ margin: 0, marginBottom: "0.8rem" }} readOnly={true} />
-                        <span className="selectProject">Select Project</span>
-                        <br />
-                        <select onChange={e => selctChange(e)} className="addTaskDropDown" style={{ margin: 0, marginBottom: "0.8rem"  }} ><option value={""}>Select</option>{assignedProject.map((v, i) => <option key={i} value={v}>{v}</option>)}</select>
-                        <button type={"button"} className="add__Task__Btn" disabled={disabled} onClick={() => editPage ? handleUpdateTaskBtnClick() : CreateNewTaskFunction()}>{editPage ? "Update Issue" : "Add Issue"}</button>
-                    </> :
-
-                        <>
-                            {
-                                teamMembers.length < 1 ? <>
-                                    <h4>Your team members will appear here</h4>
-                                </> :
-                                    <>
-                                        <h4>Your team members ({teamMembers.length})</h4>
-                                        <div className="team__Members__Container">
-                                            {
-                                                React.Children.toArray(teamMembers.map(member => {
-                                                    return <p className="team__Member__Item" onClick={() => handleMemberItemClick(member.applicant)}>{member.applicant}</p>
-                                                }))
-                                            }
-                                        </div>
-                                    </>
-                            }
-                        </>
-                }
-            </div>
+  return (
+    <>
+      <div className="add__New__Task__Overlay">
+        <div className="add__New__Task__Container" ref={ref}>
+          <h1 className="title__Item">
+            Add New Issue
+            <AiOutlineClose
+              onClick={() => {
+                closeIssuesScreen();
+                !afterSelectionScreen && setEditPage(false);
+              }}
+              style={{ cursor: "pointer" }}
+              fontSize={"1.2rem"}
+            />
+          </h1>
+          <span className="selectProject">Enter Issue Details</span>
+          <textarea
+            placeholder="Enter Issue"
+            name={"thread"}
+            value={createIssue.thread}
+            style={{ margin: 0, marginBottom: "0.8rem" }}
+            onChange={(e) => handleChange(e.target.value, e.target.name)}
+            rows={5}
+          ></textarea>
+          <span className="selectProject">
+            Add an Image to help explain your issue better (OPTIONAL)
+          </span>
+          <input
+            type="file"
+            placeholder="Add Image"
+            onChange={handleFileChange}
+            style={{ margin: 0, marginBottom: "0.8rem" }}
+          />
+          {selectedFile && (
+            <img
+              src={URL.createObjectURL(selectedFile)}
+              alt="Uploaded Preview"
+            />
+          )}
+          <span className="selectProject">
+            Select Team to be Notified about the issue
+          </span>
+          <br />
+          <select
+            className="addTaskDropDown"
+            style={{ margin: 0, marginBottom: "0.8rem" }}
+            onChange={handleOptionChange}
+            id="team"
+            name={"team"}
+          >
+            <option value="">Select Team</option>
+            <option
+              value="Team Development A"
+              selected={optionValue === "Team Development A"}
+            >
+              Team Development A
+            </option>
+            <option
+              value="Team Development B"
+              selected={optionValue === "Team Development B"}
+            >
+              Team Development B
+            </option>
+            <option
+              value="Team Development C"
+              selected={optionValue === "Team Development C"}
+            >
+              Team Development C
+            </option>
+            <option
+              value="Team Development D"
+              selected={optionValue === "Team Development D"}
+            >
+              Team Development D
+            </option>
+          </select>
+          <button
+            type={"button"}
+            className="add__Task__Btn"
+            disabled={disabled}
+            onClick={(e) => handleCreateIssue(e)}
+          >
+            {editPage ? "Update Issue" : "Add Issue"}
+          </button>
         </div>
+      </div>
     </>
-}
+  );
+};
 
 export default AddIssueScreen;
