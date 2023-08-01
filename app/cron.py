a6ef198d6c109eabcbe2b6f560dcee030a109490
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-01 22:09:47 +0545

    public job apply added

--- app/cron.py
+++ app/cron.py
@@ -15,22 +15,15 @@ def change_status(self, *args,):
         "status": "hired"
     }
     applications = dowellconnection(*candidate_management_reports, "fetch", field, update_field)
-    job_applications_data=json.load(application)
+    job_applications_data=json.load(applications)
     for application in job_applications_data:
             hired_on = application.get("hired_on")
-            if hired_on:
-                rehire_date = hired_on + relativedelta(days=7)
-                current_date = datetime.today()
-                if current_date < rehire_date:
-                    application["status"] = 'Hired'
-                else:
-                    application["status"] = 'Rehire'
-    
-    # insert_response = dowellconnection(
-    #         *candidate_management_reports, "update", field, update_field
-    #     )
-    data["job_applications"]=len(json.loads(job_applications)['data'])
-    Hired = dowellconnection(*candidate_management_reports, "fetch", {"status": "hired"}, update_field)
+            rehire_date = hired_on + relativedelta(days=7)
+            current_date = datetime.today()
+            if current_date < rehire_date:
+                application["status"] = 'Hired'
+            else:
+                application["status"] = 'Rehire'
+   
 
-change_status();
         
