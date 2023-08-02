7d8cbcd8c670d2eb400d0f4af9bd6b957d79f47a
codeftshaunak <sagardeyyash19@gmail.com>
2023-08-03 02:37:29 +0600

    update image url send

--- src/pages/TeamleadPage/views/AddIssueScreen/AddIssueScreen.js
+++ src/pages/TeamleadPage/views/AddIssueScreen/AddIssueScreen.js
@@ -9,6 +9,8 @@ import { useCurrentUserContext } from "../../../../contexts/CurrentUserContext";
 import { createCandidateTask } from "../../../../services/candidateServices";
 import { toast } from "react-toastify";
 import { createThread } from "../../../../services/threadServices";
+import { createTeam, createTeamTask, getAllTeams } from '../../../../services/createMembersTasks'
+
 
 const AddIssueScreen = ({
   teamMembers,
@@ -34,7 +36,10 @@ const AddIssueScreen = ({
     image: "",
     team_alearted_id: "",
     created_by: currentUser.userinfo.username,
+    team_id: '',
+    previous_status: '',
   });
+
 
   useClickOutside(ref, () => {
     closeIssuesScreen();
@@ -58,14 +63,64 @@ const AddIssueScreen = ({
     });
   };
 
-  const handleFileChange = (e) => {
-    setSelectedFile(e.target.files[0]);
-    setCreateIssue((prev) => {
-      const newCreateIssue = { ...prev };
-      newCreateIssue["image"] = e.target.files[0];
-      return newCreateIssue;
-    });
+  const [teamdata, setTeamData] = useState([])
+  const filteredData = teamdata.filter(item => item.admin_team === true);
+  //get all teams
+  const getTeams = async () => {
+    try {
+      const id = currentUser.portfolio_info[0].org_id;
+
+      // Call the getAllTeams function with the id
+      const response = await getAllTeams(id);
+
+      // Assuming the response contains the teams data in the data field
+      const teamsData = response.data.response.data;
+      setTeamData(teamsData)
+    } catch (error) {
+      console.error('Error in your function:', error);
+    }
   };
+
+  // Call your function to use the data received from getAllTeams
+  getTeams();
+
+  const handleFileChange = async (e) => {
+    const selectedFile = e.target.files[0];
+
+    // Create a FormData object to send the file
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    try {
+      // Send a POST request to the upload URL
+      const response = await fetch("http://67.217.61.253/uploadfiles/upload-hr-image/", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        // If the request is successful, parse the JSON response
+        const data = await response.json();
+        console.log(data);
+        // Assuming the response contains a field called "imageUrl"
+        const imageUrl = data.file_url;
+
+        // Update the createIssue state with the received image URL
+        setCreateIssue((prev) => {
+          const newCreateIssue = { ...prev };
+          newCreateIssue["image"] = imageUrl;
+          return newCreateIssue;
+        });
+      } else {
+        // Handle the case where the request is not successful
+        console.error("Error uploading image");
+      }
+    } catch (error) {
+      // Handle any errors that occurred during the request
+      console.error("Error uploading image", error);
+    }
+  };
+
 
   useEffect(() => {
     if (createIssue.thread.length < 1) return setShowIssueForm(false);
@@ -82,15 +137,17 @@ const AddIssueScreen = ({
     // console.log(createIssue);
 
     try {
-        const response = await createThread(createIssue);
-        console.log(response.data);
-        if (response.status === 201) {
-            toast.success("Issue Created Successfully");
-        }
+      const response = await createThread(createIssue);
+      console.log(response.data);
+      if (response.status === 201) {
+        toast.success("Issue Created Successfully");
+      }
     } catch (error) {
-        toast.error("Something went wrong");
+      toast.error("Something went wrong");
     }
   };
+
+
 
   return (
     <>
