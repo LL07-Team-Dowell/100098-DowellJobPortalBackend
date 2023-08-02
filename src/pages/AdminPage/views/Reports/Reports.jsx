52d59cca6d06ff49ba1a7eb95ca635e51055f743
alaasassii <alaasassjs@gmail>
2023-08-02 17:36:13 +0200

    create a new function for general report in admin server

--- src/pages/AdminPage/views/Reports/Reports.jsx
+++ src/pages/AdminPage/views/Reports/Reports.jsx
@@ -1,24 +1,32 @@
-import StaffJobLandingLayout from "../../../../layouts/StaffJobLandingLayout/StaffJobLandingLayout"
+import React from "react";
+import StaffJobLandingLayout from "../../../../layouts/StaffJobLandingLayout/StaffJobLandingLayout";
 
-const AdminReports = ({subAdminView}) => {
-    // states
-    const [selectOptions, setSelectOptions] = React.useState('') ; 
+const AdminReports = ({ subAdminView }) => {
+  // states
+  const [selectOptions, setSelectOptions] = React.useState("");
 
-    // handle functions 
-    const handleSelectOptionsFunction = (e) => {
-        setSelectOptions(e.target.value)
-    }
-    return <StaffJobLandingLayout adminView={true} adminAlternativePageActive={true} pageTitle={"Reports"} subAdminView={subAdminView}>
-           <div className="reports__container">
-            <h3>Reports</h3>
-            <p>Get insights into your organizations</p>
-            <select onChange={handleSelectOptionsFunction}>
-                <option value="select_time">select time</option>
-                <option value="last_7_days">last 7 days</option>
-                <option value="custom_time">cutom time</option>
-            </select>
-           </div>
+  // handle functions
+  const handleSelectOptionsFunction = (e) => {
+    setSelectOptions(e.target.value);
+  };
+  return (
+    <StaffJobLandingLayout
+      adminView={true}
+      adminAlternativePageActive={true}
+      pageTitle={"Reports"}
+      subAdminView={subAdminView}
+    >
+      <div className="reports__container">
+        <h3>Reports</h3>
+        <p>Get insights into your organizations</p>
+        <select onChange={handleSelectOptionsFunction}>
+          <option value="select_time">select time</option>
+          <option value="last_7_days">last 7 days</option>
+          <option value="custom_time">cutom time</option>
+        </select>
+      </div>
     </StaffJobLandingLayout>
-}
+  );
+};
 
 export default AdminReports;
