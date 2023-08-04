85a4ebdf7aa3cfa057b83b2089c1e224e9cae9af
hardik <c.hardik125@gmail.com>
2023-08-04 21:33:02 +0530

    admin superuser created

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import SettingUserProfileInfo
+from .models import SettingUserProfileInfo,UserProject
 
 # Register your models here.
 
 # settings models registered______________________________________
 admin.site.register(SettingUserProfileInfo)
-
+admin.site.register(UserProject)
