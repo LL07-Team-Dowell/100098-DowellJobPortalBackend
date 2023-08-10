a48d3e5eb43ee56d038f61afea0ab88ed6d6fec3
ala sassi <alaasassijs@gmail.com>
2023-08-10 19:57:21 +0200

    pull from frontend branch

--- src/pages/AdminPage/views/Reports/Reports.jsx
+++ src/pages/AdminPage/views/Reports/Reports.jsx
@@ -370,7 +370,7 @@ const FormDatePopup = ({
 export default AdminReports;
 function formatDateFromMilliseconds(milliseconds) {
   const dateObj = new Date(milliseconds);
-
+  // comment
   const month = String(dateObj.getMonth() + 1).padStart(2, "0");
   const day = String(dateObj.getDate()).padStart(2, "0");
   const year = dateObj.getFullYear();
