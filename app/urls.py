0a181c6a212a5a4b3b04d2227db893d1b5389e11
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-10 10:30:39 +0545

    get request url changed

--- app/urls.py
+++ app/urls.py
@@ -93,6 +93,7 @@ urlpatterns = [
 
     # Thread-------------------------------------
     path('fetch_thread/',Thread_Apis.as_view()),
+    path('fetch_thread/<str:document_id>/',Thread_Apis.as_view()),
     path('fetch_team_thread/<str:team_id>/',GetTeamThreads.as_view()),
     path('fetch_all_threads/',GetAllThreads.as_view()),
     path('create_thread/',Thread_Apis.as_view()),
@@ -100,6 +101,7 @@ urlpatterns = [
 
     # Comment-------------------------------------
     path('fetch_comment/',Comment_Apis.as_view()),
+    path('fetch_comment/<str:document_id>/',Comment_Apis.as_view()),
     path('create_comment/',Comment_Apis.as_view()),
     path('update_comment/', Comment_Apis.as_view()),
 
