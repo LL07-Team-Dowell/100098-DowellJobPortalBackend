c3abb91f36efc352d179afeb82ee89724787135e
Ayoola <ayo.oloyede16@gmail.com>
2023-09-05 16:36:52 +0100

    update

--- src/pages/TeamleadPage/views/TaskScreen/TaskScreen.js
+++ src/pages/TeamleadPage/views/TaskScreen/TaskScreen.js
@@ -219,6 +219,7 @@ const TaskScreen = ({
     // Add class to tiles in month view only
     if (view === "month") {
       // Check if a date React-Calendar wants to check is on the list of dates to add class to
+      if (tasksForProjectLoading) return ''
       if (datesToStyle.find((dDate) => isSameDay(dDate, date))) {
         return "task__Indicator";
       }
