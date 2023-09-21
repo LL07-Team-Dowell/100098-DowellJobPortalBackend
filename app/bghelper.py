950c208a8d699090154afa676620484d2a4e097a
hardik <c.hardik125@gmail.com>
2023-09-21 15:06:33 +0530

    changes done on helper function

--- app/bghelper.py
+++ app/bghelper.py
@@ -1,23 +1,34 @@
 import json
 import requests
-def dowellconnection(cluster, database, collection, document, team_member_ID, function_ID, command, field,
-                     update_field):
+
+
+def dowellconnection(
+    cluster,
+    database,
+    collection,
+    document,
+    team_member_ID,
+    function_ID,
+    command,
+    field,
+    update_field,
+):
     url = "http://uxlivinglab.pythonanywhere.com"
-    payload = json.dumps({
-        "cluster": cluster,
-        "database": database,
-        "collection": collection,
-        "document": document,
-        "team_member_ID": team_member_ID,
-        "function_ID": function_ID,
-        "command": command,
-        "field": field,
-        "update_field": update_field,
-        "platform": "bangalore"
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    payload = json.dumps(
+        {
+            "cluster": cluster,
+            "database": database,
+            "collection": collection,
+            "document": document,
+            "team_member_ID": team_member_ID,
+            "function_ID": function_ID,
+            "command": command,
+            "field": field,
+            "update_field": update_field,
+            "platform": "bangalore",
+        }
+    )
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
     res = json.loads(response.text)
