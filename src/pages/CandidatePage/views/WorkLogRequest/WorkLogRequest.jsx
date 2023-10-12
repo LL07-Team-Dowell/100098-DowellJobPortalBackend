98318816033e670b33f75d337cbc7b5d4b90dea8
alaasassii <alaasassijs@gmail.com>
2023-10-12 22:16:19 +0100

    add new things

--- src/pages/CandidatePage/views/WorkLogRequest/WorkLogRequest.jsx
+++ src/pages/CandidatePage/views/WorkLogRequest/WorkLogRequest.jsx
@@ -4,9 +4,11 @@ import { useCurrentUserContext } from "../../../../contexts/CurrentUserContext";
 import JobLandingLayout from "../../../../layouts/CandidateJobLandingLayout/LandingLayout";
 import { useGetAllUpdateTask } from "./hook/useGetAllUpdateTask";
 import LoadingSpinner from "../../../../components/LoadingSpinner/LoadingSpinner";
+
 const WorkLogRequest = () => {
   const { currentUser } = useCurrentUserContext();
   const { data, loading, error } = useGetAllUpdateTask(currentUser);
+
   if (error) return <h1>{error}</h1>;
   if (loading) return <LoadingSpinner />;
   return (
