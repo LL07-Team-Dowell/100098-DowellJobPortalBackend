7bd22a1c7b5c63005b9b53d4c3a0aa5b67bf3719
Manish <mdashsharma95@gmail.com>
2023-10-12 16:46:48 +0530

    added beta verion URL to CORS ORIGIN

--- core/settings.py
+++ core/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = ["ll07-team-dowell.github.io/100098-DowellJobPortal/"]
 # CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "core.urls"
