34775db96177c287f13a8bf3127e8c8b5963a2e2
alaasassii <alaasassijs@gmail.com>
2023-10-17 18:20:20 +0100

    add icon

--- src/pages/TeamleadPage/views/CreateMembersTask/component/Teams.jsx
+++ src/pages/TeamleadPage/views/CreateMembersTask/component/Teams.jsx
@@ -6,7 +6,7 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 // Fetch Teams for that company
 import { teams, imageReturn } from "../assets/teamsName";
 import { useNavigate } from "react-router-dom";
-import { AiOutlineClose, AiOutlineTeam } from "react-icons/ai";
+import { AiFillEdit, AiOutlineClose, AiOutlineTeam } from "react-icons/ai";
 import { deleteTeam } from "../../../../../services/createMembersTasks";
 import { Tooltip } from "react-tooltip";
 import { RiEdit2Fill } from "react-icons/ri";
@@ -108,12 +108,33 @@ const Team = ({
           />
         </div>
       )}
+      {v.created_by === currentUser.userinfo.username && (
+        <div
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 35,
+            width: 18,
+            zIndex: 999,
+            cursor: "pointer",
+          }}
+          onClick={showDeletePopup}
+          data-tooltip-id={v._id}
+          data-tooltip-content={"Edit"}
+        >
+          <AiFillEdit style={{ fontSize: "1.3rem", color: "#000" }} />
+          <Tooltip
+            id={v._id}
+            style={{ fontSize: "0.7rem", fontWeight: "normal" }}
+          />
+        </div>
+      )}
       {v.admin_team && (
         <div
           style={{
             position: "absolute",
             top: 12,
-            right: 30,
+            right: 60,
             width: "fit-content",
             zIndex: 999,
             cursor: "pointer",
