f35d24365b009a7ce86bd2a23e4474fc3def94f7
Blackbullet99 <isaacokanigbe@gmail.com>
2023-11-04 08:33:14 +0100

    fixed issue with image url not being shown in table

--- src/pages/TeamleadPage/views/AddTaskScreen/AddTaskScreen.js
+++ src/pages/TeamleadPage/views/AddTaskScreen/AddTaskScreen.js
@@ -582,7 +582,7 @@ const AddTaskScreen = ({
       "subproject": subprojectSelected,
     }
 
-    if (imageUrl.length > 0) dataToPost.Task_image = imageUrl;
+    if (imageUrl.length > 0) dataToPost.task_image = imageUrl;
 
     const dataToPost2 = {
       "company_id": currentUser.portfolio_info[0].org_id,
@@ -1194,7 +1194,7 @@ const AddTaskScreen = ({
                                     <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.start_time}</td>
                                     <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.end_time}</td>
                                     <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.task}</td>
-                                    <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.Task_image ? selectedFile : "None"}</td> 
+                                    <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.task_image ? task.task_image : 'None'}</td> 
                                     <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.task_type}</td>
                                     <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.subproject}</td>
                                     <td className={task.is_active && task.is_active === true ? "" : "deleted"}>{task.project}</td>
