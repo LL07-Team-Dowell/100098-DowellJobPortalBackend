7c36a7578c3aae5af4d291db23035514dfe536ce
Ayoola <ayo.oloyede16@gmail.com>
2023-11-22 07:55:31 +0100

    update log requests page for project lead

--- src/pages/ProjectLeadPage/views/LogRequests/ProjectLogRequest.jsx
+++ src/pages/ProjectLeadPage/views/LogRequests/ProjectLogRequest.jsx
@@ -5,11 +5,46 @@ import WorkLogRequest from "../../../TeamleadPage/views/WorkLogRequest/WorkLogRe
 import { useCurrentUserContext } from "../../../../contexts/CurrentUserContext";
 import StaffJobLandingLayout from "../../../../layouts/StaffJobLandingLayout/StaffJobLandingLayout";
 import TitleNavigationBar from "../../../../components/TitleNavigationBar/TitleNavigationBar";
+import { useCandidateTaskContext } from "../../../../contexts/CandidateTasksContext";
+import { useEffect } from "react";
+import { getSettingUserProject } from "../../../../services/hrServices";
 
 const ProjectLogRequest = () => {
     const { currentUser } = useCurrentUserContext();
     const navigate = useNavigate();
   const [cardData, setCardData] = useState("Pending approval");
+  const {
+    allProjects,
+    setAllProjects,
+  } = useCandidateTaskContext();
+
+  useEffect(() => {
+    if (allProjects.length > 0) return
+
+    getSettingUserProject().then((res) => {
+      const projectsGotten = res.data
+      ?.filter(
+        (project) =>
+          project?.data_type === currentUser.portfolio_info[0].data_type &&
+          project?.company_id === currentUser.portfolio_info[0].org_id &&
+          project.project_list &&
+          project.project_list.every(
+            (listing) => typeof listing === "string"
+          )
+      )
+      ?.reverse()
+      
+      let allProjectsFetched;
+
+      if (projectsGotten.length > 0) {
+        allProjectsFetched = projectsGotten[0]?.project_list.sort((a, b) => a.localeCompare(b));
+        setAllProjects(allProjectsFetched);
+      }
+    }).catch(err => {
+      console.log('err fetching projects for project lead: ', err?.response?.data);
+    })
+  }, [])
+
   // sadsadasd
   return (
     <StaffJobLandingLayout projectLeadView={true} hideSearchBar={true}>
@@ -48,7 +83,11 @@ const ProjectLogRequest = () => {
             Denied
           </NavLink>
         </Wrappen>
-        <WorkLogRequest cardData={cardData} />
+        <WorkLogRequest 
+          cardData={cardData} 
+          isProjectLead={true}
+          allProjectsForLeadPassed={allProjects}
+        />
       </div>
     </StaffJobLandingLayout>
   );
