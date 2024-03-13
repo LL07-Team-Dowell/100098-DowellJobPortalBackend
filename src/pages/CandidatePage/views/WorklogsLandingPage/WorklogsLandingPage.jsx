af8915114e084b970d77e9e7e3aa4760349c7edf
Ayoola <ayo.oloyede16@gmail.com>
2024-03-13 10:52:03 +0100

    Updates and fixes

--- src/pages/CandidatePage/views/WorklogsLandingPage/WorklogsLandingPage.jsx
+++ src/pages/CandidatePage/views/WorklogsLandingPage/WorklogsLandingPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import StaffJobLandingLayout from "../../../../layouts/StaffJobLandingLayout/StaffJobLandingLayout";
 import { useNavigate } from "react-router-dom";
-import { TfiAgenda } from "react-icons/tfi";
 import styles from './styles.module.css';
-import { TbReportAnalytics } from "react-icons/tb";
-import { MdOutlineUpdate } from "react-icons/md";
+import { AiOutlineAim } from "react-icons/ai";
 
 const WorklogsLandingPage = () => {
     const navigate = useNavigate();
+
     return (
         <div className={`${styles.create_team_parent} ${styles.report}`} style={{ padding: '20px 20px 200px' }}>
             <div
@@ -16,11 +14,13 @@ const WorklogsLandingPage = () => {
             >
                 <div>
                     <div>
-                        <TbReportAnalytics className={styles.icon} />
+                        <AiOutlineAim
+                            className={styles.icon}
+                        />
                     </div>
                     <h4>View Worklogs</h4>
                     <p>
-                        Get insights into attendance trends and weekly progress for effective organization management.
+                        View your tasks given and milestones completed on a project.
                     </p>
                 </div>
             </div>
@@ -30,11 +30,13 @@ const WorklogsLandingPage = () => {
             >
                 <div>
                     <div>
-                        <MdOutlineUpdate className={styles.icon} />
+                        <AiOutlineAim
+                            className={styles.icon}
+                        />
                     </div>
                     <h4>Log Request</h4>
                     <p>
-                        Update attendance records for precise tracking and efficient management of working hours.
+                        Track the progress and status of worklog requests you made
                     </p>
                 </div>
             </div>
